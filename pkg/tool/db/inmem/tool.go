57d9fff3c8bcef365f84a28d76a3092ab7d42390
nbeck <niklas.beck@iais.fraunhofer.de>
2025-02-08 09:56:13 +0000

    Clean up inmem tool db
    * Use sync.Map
    * Remove logger and tracer

--- pkg/tool/db/inmem/tool.go
+++ pkg/tool/db/inmem/tool.go
@@ -2,12 +2,8 @@ package inmem
 
 import (
 	"context"
-	"log/slog"
 	"sync"
 
-	"go.opentelemetry.io/otel/trace"
-
-	"github.com/Br0ce/opera/pkg/monitor"
 	"github.com/Br0ce/opera/pkg/tool"
 	"github.com/Br0ce/opera/pkg/tool/db"
 )
@@ -15,77 +11,59 @@ import (
 var _ tool.DB = (*Tool)(nil)
 
 type Tool struct {
-	tools map[string]tool.Tool
-	mu    sync.RWMutex
-	tr    trace.Tracer
-	log   *slog.Logger
+	tools sync.Map
 }
 
-func NewDB(tracer trace.Tracer, log *slog.Logger) *Tool {
-	return &Tool{
-		tools: make(map[string]tool.Tool),
-		tr:    tracer,
-		log:   log,
-	}
+func NewDB() *Tool {
+	return &Tool{}
 }
 
 func (to *Tool) Add(ctx context.Context, tool tool.Tool) error {
-	_, span := to.tr.Start(ctx, "add tool")
-	defer span.End()
-	to.log.Debug("add tool", "method", "Add", "name", tool.Name(), "traceID", monitor.TraceID(span))
-
-	to.mu.Lock()
-	defer to.mu.Unlock()
-
-	_, ok := to.tools[tool.Name()]
+	_, ok := to.tools.LoadOrStore(tool.Name(), tool)
 	if ok {
 		return db.ErrAlreadyExists
 	}
-
-	to.tools[tool.Name()] = tool
-
 	return nil
 }
 
 func (to *Tool) Get(ctx context.Context, name string) (tool.Tool, error) {
-	_, span := to.tr.Start(ctx, "get tool")
-	defer span.End()
-	to.log.Debug("get tool", "method", "Get", "name", name, "traceID", monitor.TraceID(span))
-
-	to.mu.RLock()
-	defer to.mu.RUnlock()
-
 	if name == "" {
 		return tool.Tool{}, db.ErrInvalidName
 	}
 
-	t, ok := to.tools[name]
+	v, ok := to.tools.Load(name)
 	if !ok {
 		return tool.Tool{}, db.ErrNotFound
 	}
 
+	t, ok := v.(tool.Tool)
+	if !ok {
+		return tool.Tool{}, db.ErrInternal
+	}
 	return t, nil
 }
 
 func (to *Tool) All(ctx context.Context) ([]tool.Tool, error) {
-	_, span := to.tr.Start(ctx, "get all tools")
-	defer span.End()
-	to.log.Debug("get all tools", "method", "All", "traceID", monitor.TraceID(span))
+	all := true
+	var tt []tool.Tool
+	to.tools.Range(func(_, value any) bool {
+		t, ok := value.(tool.Tool)
+		if !ok {
+			all = false
+			return false
+		}
 
-	tt := make([]tool.Tool, 0, len(to.tools))
-	for _, t := range to.tools {
 		tt = append(tt, t)
-	}
+		return true
+	})
 
+	if !all {
+		return nil, db.ErrInternal
+	}
 	return tt, nil
 }
 
 func (to *Tool) Clear(ctx context.Context) error {
-	_, span := to.tr.Start(ctx, "delete all tools")
-	defer span.End()
-	to.log.Debug("delete all tools", "method", "clear", "traceID", monitor.TraceID(span))
-
-	clear(to.tools)
-	to.tools = make(map[string]tool.Tool)
+	to.tools.Clear()
 	return nil
 }
